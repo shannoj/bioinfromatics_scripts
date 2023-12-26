dd4ef337d3fcde2bcf199cd6bb34031a707b1be9
shannoj <69207315+shannoj@users.noreply.github.com>
2023-12-26 18:46:07 +0100

    script trims and aligns

--- rnaSeqPipline.sh
+++ rnaSeqPipline.sh
@@ -2,6 +2,6 @@
 
 cd /Users/jamesshannon/Desktop/Bioinfromatics/
 
-trimmomatic SE -threads 4 -phred33  fastq-files/fastq-files/fastq-files/ERR101899_1.fastq.gz fastq_files/trim_output/output2.trimmed.paired.R1.fastq.gz LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+trimmomatic SE -threads 4 -phred33  fastq_files/fastq_files/fastq_files/ERR101899_1.fastq.gz fastq_files/trim_output/output2.trimmed.paired.R1.fastq.gz LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
-./HISAT2/hisat2-2.2.1/hisat2 -q --rna-strandness R -x ./HISAT2/hisat2-2.2.1/indexes/GRCh38.p13.genome -U fastq_files/trim_output/output2.trimmed.paired.R1.fastq.gz | ./samtools/bin/samtools sort -o fastq_files/align_output/output2.trimmed.paired.R1.bam
+./HISAT2/hisat2-2.2.1/hisat2 -q --rna-strandness R -x ./HISAT2/grch38/grch38/genome -U fastq_files/trim_output/output2.trimmed.paired.R1.fastq.gz | ./samtools/bin/samtools sort -o ./Bam_files/output2.trimmed.paired.R1.bam
