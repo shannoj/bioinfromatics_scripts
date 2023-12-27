d738b31cb6e8e86c7419ad18e2b3618b7f4b0b90
shannoj <69207315+shannoj@users.noreply.github.com>
2023-12-27 10:34:26 +0100

    added feature counts tool to get the counts

--- rnaSeqPipline.sh
+++ rnaSeqPipline.sh
@@ -8,3 +8,6 @@ trimmomatic SE -threads 4 -phred33  fastq_files/fastq_files/fastq_files/ERR10189
 
 # Using HISAT2 to align the trimmed fastq files to the reference genome
 ./HISAT2/hisat2-2.2.1/hisat2 -q --rna-strandness R -x ./HISAT2/grch38/grch38/genome -U fastq_files/trim_output/output2.trimmed.paired.R1.fastq.gz | ./samtools/bin/samtools sort -o ./Bam_files/output2.trimmed.paired.R1.bam
+
+# Getting counts using feature counts 
+./subread-2.0.6-macOS-x86_64/bin/featureCounts -S 2 -a ./ensmbl_homo_sapiens/Homo_sapiens.GRCh38.110.gtf.gz -o ./feature_counts/demo_featurecounts.txt ./Bam_files/output2.trimmed.paired.R1.bam
